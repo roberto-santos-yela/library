945e96c6f60a920832258668119ae66924127c03
ROBERTO_SANTOS APPS1MA1819 <roberto_santos_apps1ma1819@cev.com>
2019-10-29 14:30:50 +0100

    users_lend_books_table_added

---
+++ database/migrations/2019_10_29_132327_create_users_lend_books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersLendBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_lend_books', function (Blueprint $table) {
+            $table->primary('user_id','book_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_lend_books');
+    }
+}

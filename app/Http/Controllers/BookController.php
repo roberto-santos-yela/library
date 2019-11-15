12edbd48ab5752c51a477363f76de85a4fbd88c5
ROBERTO_SANTOS APPS1MA1819 <roberto_santos_apps1ma1819@cev.com>
2019-11-15 11:46:36 +0100

    book_controller_added

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,23 +7,86 @@ use App\Book;
 
 class BookController extends Controller
 {
-    public function create(Request $request)
-    {
-        $books = new Book();
-        $books->title = $request->input('title');
-        $books->description = $request->input('description');
-
-        $books->save();
-        return response()->json($books);
-
-    }
-
-    public function getBooks(Book $books)
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Book $books)
     {
         $books = Book::all();
         return response()->json($books);
 
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $books = new Book();
+        $books->title = $request->input('title');
+        $books->description = $request->input('description');
+        $books->save();
+   
+        return response()->json($books);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
